ff5b4bc39bc726f6efb9f4b978a284d254ab39bc
Alegherix <psao69@gmail.com>
2021-02-16 16:35:06 +0100

    removed unecessary links

--- src/constants/personalProjects.js
+++ src/constants/personalProjects.js
@@ -1,46 +1,26 @@
 export default [
   {
-    "src" : "1-Sniper.jpg",
-    "name": "Tradera Budsniper",
-    "desc": "Ett program som är till för att vinna auktioner på Tradera igenom att vänta tills det återstår 1 sekund kvar av auktionen, för att sedan lägga ett bud och se till att ingen kan återbuda upp auktionen högre.",
-    "btnText" : "Github",
-    "url": "https://github.com/psychoslaz/YoutubeTraderaSniper"
+    src: "1-Sniper.jpg",
+    name: "Tradera Budsniper",
+    desc:
+      "Ett program som är till för att vinna auktioner på Tradera igenom att vänta tills det återstår 1 sekund kvar av auktionen, för att sedan lägga ett bud och se till att ingen kan återbuda upp auktionen högre.",
+    btnText: "Github",
+    url: "https://github.com/psychoslaz/YoutubeTraderaSniper",
   },
   {
-    "src": "edgeLedger.png",
-    "name": "EdgeLedger Consulting",
-    "desc": "En hemsida för EdgeLedger Consulting. Ett testprojekt för ett nystart konsult företag med inriktning på private banking ",
-    "btnText": "Besök",
-    "url": "http://edge-ledger.surge.sh"
+    src: "edgeLedger.png",
+    name: "EdgeLedger Consulting",
+    desc:
+      "En hemsida för EdgeLedger Consulting. Ett testprojekt för ett nystart konsult företag med inriktning på private banking ",
+    btnText: "Besök",
+    url: "http://edge-ledger.surge.sh",
   },
   {
-    "src": "2-firstSite.png",
-    "name": "Första portföljen",
-    "desc": "En utav de första webbsidor jag skrev för att kunna komma i kontakt med mig.",
-    "btnText": "Besök",
-    "url": "https://psychoslaz.github.io/portfolio/"
+    src: "2-firstSite.png",
+    name: "Första portföljen",
+    desc:
+      "En utav de första webbsidor jag skrev för att kunna komma i kontakt med mig.",
+    btnText: "Besök",
+    url: "https://psychoslaz.github.io/portfolio/",
   },
-  // {
-  //   "src": "2-hauntedHouse.jpg",
-  //   "name": "Hemsökta hus",
-  //   "desc": "En hemsida för att boka tourguider med resa, boende och mat till några av Sveriges mest hemsökta hus.",
-  //   "btnText" : "Besök",
-  //   "url": "http://www.hauntedtours.se"
-  // },
- 
-  // {
-  //   "src": "cake2.jpg",
-  //   "name": "Café Regnbågen",
-  //   "desc": "En hemsida för Café Regnbågen.",
-  //   "btnText" : "Besök",
-  //   "url": "http://caferegnbågen.se",
-  // },
-  //  {
-  //   "src":"5-hotel.jpg",
-  //   "name": "Hotell Varda",
-  //   "desc": "En hemsida för Hotell Varda, där folk enkelt kan få en översikt över hotellet, och även se till att komma i kontakt, för att reservera en bokning.",
-  //   "btnText" : "Besök",
-  //   "url": "http://hotellvarda.se",
-  // },/home/psychoslaz/Development/Gatsby/MinBloggResponsive/src/images/jagRund.jpg
-
 ]
